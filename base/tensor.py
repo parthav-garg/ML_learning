87415625ebfc064e27eb553de3800e7aba259873
shadowquwu <shadowsinthenight79@gmail.com>
2025-07-09 21:56:55 +0530

    add and multiply function sof Tensor

--- base/tensor.py
+++ base/tensor.py
@@ -2,13 +2,31 @@ import numpy as np
 
 
 class Tensor:
-    def __init__(self, data, _children):
+    def __init__(self, data, _prev=set()):
         self._data = np.array(data)
-        self._children = set()
+        self._prev = _prev
         self.backward = lambda: None
-        self.grad = np.zeros_like(self._data)
+        self._grad = np.zeros_like(self._data)
     def __add__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
-        c = Tensor(self._data + other._data, )
+        other = other if isinstance(other, Tensor) else Tensor(other, ())
+        c = Tensor(self._data + other._data, _prev=(self, other))
+        def backward_fn():
+            print("self=", self._data)
+            print("other=", other._data)
+            self._grad += 1 * c._grad
+            other._grad += 1 * c._grad
+        c.backward = backward_fn
+        return c
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other, ())
+        c = Tensor(self._data * other._data, _prev=(self, other))
+        def backward_fn():
+            self._grad += other._data * c._grad
+            other._grad += self._data * c._grad
+        c.backward = backward_fn
+    
         
-        
+    def __repr__(self):
+        return self._data.__str__()
+    def __str__(self):
+        return self._data.__str__()
--- git_script.sh
+++ git_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is used to stage, commit, and push changes to the Git repository.
-if git add . && git commit -m "Update files" && git push origin main; then
+if git add . && git commit -m "$1" && git push origin main; then
   echo "Changes pushed successfully"
 else
   echo "Failed to push changes"
