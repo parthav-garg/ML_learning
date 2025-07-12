fbd05d052d32a49fcfddfd5f28736091481efc40
shadowquwu <shadowsinthenight79@gmail.com>
2025-07-12 11:16:43 +0530

    a lil bit more before we start nn modules

--- base/tensor.py
+++ base/tensor.py
@@ -1,32 +1,47 @@
 import numpy as np
 
+_graph = []
 
 class Tensor:
     def __init__(self, data, _prev=set()):
         self._data = np.array(data)
         self._prev = _prev
-        self.backward = lambda: None
+        self._backward = lambda: None
         self._grad = np.zeros_like(self._data)
+        _graph.append(self)
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other, ())
         c = Tensor(self._data + other._data, _prev=(self, other))
         def backward_fn():
-            print("self=", self._data)
-            print("other=", other._data)
             self._grad += 1 * c._grad
             other._grad += 1 * c._grad
-        c.backward = backward_fn
+        c._backward = backward_fn
         return c
+    
     def __mul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other, ())
         c = Tensor(self._data * other._data, _prev=(self, other))
         def backward_fn():
             self._grad += other._data * c._grad
             other._grad += self._data * c._grad
-        c.backward = backward_fn
+        c._backward = backward_fn
     
+    def __matmul__(self, other):
+        c = Tensor(self._data @  other._data, _prev=(self, other))
+        def backward_fn():
+            self._grad += c._grad @ other._data.transpose()
+            other._grad += self._data @ c._grad
+        c._backward = backward_fn
+    
+    def __div__(self, other):
+        c = Tensor(self._data/ other._data, _prev=(self, other))
         
+    def backward(self):
+        for node in reversed(_graph):
+            node._backward()
+    
     def __repr__(self):
         return self._data.__str__()
+    
     def __str__(self):
         return self._data.__str__()
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from base.tensor import Tensor
 import numpy as np
 if __name__ == "__main__":
-    a = Tensor(np.array([1,2,3,4,5]), ())
-    b = Tensor(np.array([1,2,3,4,5]), ())
-    c = a + b
-    d = Tensor(np.array([1,1,1,1,1]), ())
-    e = c + d
-    e.grad = np.array([1,2,1,2,1])
-    print(d.grad)
-    print(c.grad)
-    print(a.grad)
-    print(b.grad)
+    
     
